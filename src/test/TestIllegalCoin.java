f680825691c4ee7ee0e6a7ea5553cd474ea220ec
Skyttegaard <10461236@ucn.dk>
2023-02-22 13:16:51 +0100

    Updated validation test
    Tests made for validation of illiegal coins

--- src/test/TestIllegalCoin.java
+++ src/test/TestIllegalCoin.java
@@ -1,12 +1,18 @@
 package test;
 
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.*;
 
 import controllayer.*;
+import modellayer.Coin;
+import modellayer.Currency;
+import utility.Validation;
 
 /**
- * Inspired by the book: Flexible, Reliable Software Henrik Bærbak Christensen:
+ * Inspired by the book: Flexible, Reliable Software Henrik Bï¿½rbak Christensen:
  * Flexible, Reliable Software. Taylor and Francis Group, LLC 2010
  */
 
@@ -27,11 +33,26 @@ public class TestIllegalCoin {
 	// Norwegian coin
 	@Test(expected = IllegalCoinException.class)
 	public void shouldRejectIllegalCurrencyNokCoin() throws IllegalCoinException {
-		//
+		Coin coin;
+		int amount = 5;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.NOK;
+		
+		coin = new Coin(amount, coinCurrency, coinType);
+		
+		Validation.validateCoin(coin);
+		
 	}
 	// unknown Euro coin value
 	@Test(expected = IllegalCoinException.class)
 	public void shouldRejectIllegalEuroCoin() throws IllegalCoinException {
-		//
+		Coin coin;
+		int amount = 6;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.EURO;
+		
+		coin = new Coin(amount, coinCurrency,coinType);
+		
+		Validation.validateCoin(coin);
 	}
 }
